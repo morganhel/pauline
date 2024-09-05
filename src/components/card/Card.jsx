df87b5526047506b456debbcbe8b7bc1fbb908b2
morganhel <138800018+morganhel@users.noreply.github.com>
2024-09-05 15:13:10 +0200

    ajout plaquett prix

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,13 @@
-import { NavLink } from "react-router-dom";
+import React, {useState} from 'react';
+import Modal from '../modal/Modal';
 
-function Card({img,title,txt1,txt2,isNoBg}) {
+function Card({img,title,txt1,txt2,img2,isNoBg}) {
+    const [showModal, setShowModal]=useState(false);
+    const handleClick = () => {setShowModal(true)};
+    const closeModal = () => {setShowModal(false)};
 
     return (
+        <>
         <article className={isNoBg ? 'card nobg' : 'card'}>
             <div className="card__content">
             <h3 className='card__title'>{title}</h3>
@@ -10,11 +15,12 @@ function Card({img,title,txt1,txt2,isNoBg}) {
                     <p className='card__txt--1'>{txt1}</p>
                     <p className='card__txt--2'>{txt2}</p>
                 </div>
-            <NavLink to="/apropos#contact">
-                <button className='card__button'>+ D'INFORMATIONS !</button></NavLink>
+                <button className='card__button' onClick={handleClick}>+ D'INFORMATIONS !</button>
             </div>
-            <img className='card__img' src={img} alt='offre'/>
+            <img className='card__img' src={img2} alt='offre'/>
         </article>
+        {showModal && <Modal onClose={closeModal} img={img2}/>}
+        </>
     )
 }
 
